5114ff89e6b512892bbf5296ea6652c6984a3e2f
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-19 18:23:30 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
   get '/movies/:id/summary', to: 'movies#summary'
   get '/movie_summaries', to: 'movies#summaries'
-  resources :directors, only: [:index, :show]
-  resources :reviews, only: [:index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :directors, only: [:index, :show]  
 end
